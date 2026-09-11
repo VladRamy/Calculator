6fc8c9d5fe3fd3c2bb87f67adf4f90a06143d288
VladRamy <vladramis@mail.ru>
2026-09-11 22:30:36 +0300

    chore: настройка деплоя на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // ВАЖНО: Имя должно точно совпадать с именем репозитория на GitHub
+  base: '/Calculator/', 
 })
